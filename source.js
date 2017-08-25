002870a3acd925c4c4b6df33db0ca72db8797868
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-25 14:36:47 -0700

    Hello, dropLast

--- source.js
+++ source.js
@@ -1,3 +1,24 @@
-export default function {{NAME}} (alpha: mixed): mixed {
-  return alpha
+/* eslint-disable no-magic-numbers */
+
+import reverse from "@unction/reverse"
+import ifThenElse from "@unction/ifthenelse"
+import lessThan from "@unction/lessthan"
+import append from "@unction/append"
+import reduceWithValueKey from "@unction/reducewithvaluekey"
+import always from "@unction/always"
+
+export default function dropLast (count: number): mixed {
+  return function dropLastCount (array: ArrayType): ArrayType {
+    return reverse(
+      reduceWithValueKey(
+        (accumulated: ArrayType): Function =>
+          (value: ValueType): Function =>
+            ifThenElse(lessThan(count - 1))((): ArrayType => append(value)(accumulated))(always(accumulated))
+      )(
+        []
+      )(
+        reverse(array)
+      )
+    )
+  }
 }
