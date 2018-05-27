6a69ce03c2f16e3aa4339a23668b711b51c422f0
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:30 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
 /* eslint-disable no-magic-numbers */
-import greaterThan from "@unction/greaterthan"
-import append from "@unction/append"
-import reduceWithValueKey from "@unction/reducewithvaluekey"
-import length from "@unction/length"
-import fresh from "@unction/fresh"
+import greaterThan from "@unction/greaterthan";
+import append from "@unction/append";
+import reduceWithValueKey from "@unction/reducewithvaluekey";
+import length from "@unction/length";
+import fresh from "@unction/fresh";
+export default function dropLast(count) {
+  return function dropLastCount(orderedList) {
+    const total = length(orderedList);
+    return reduceWithValueKey(accumulated => value => index => {
+      if (greaterThan(index)(total - count - 1)) {
+        return accumulated;
+      }
 
-export default function dropLast (count: number): mixed {
-  return function dropLastCount (orderedList: ArrayType | string): ArrayType | string {
-    const total = length(orderedList)
-
-    return reduceWithValueKey(
-      (accumulated: ArrayType | string): Function =>
-        (value: ValueType): Function =>
-          (index: number): ArrayType | string => {
-            if (greaterThan(index)(total - count - 1)) {
-              return accumulated
-            }
-
-            return append(value)(accumulated)
-          }
-    )(
-      fresh(orderedList)
-    )(
-      orderedList
-    )
-  }
+      return append(value)(accumulated);
+    })(fresh(orderedList))(orderedList);
+  };
 }
